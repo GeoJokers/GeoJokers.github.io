96a417e333cd1a3d4715419a6630c4aef7058dc2
Tobias Rosskopf <tobirosskopf@gmail.com>
2022-09-16 15:44:10 +0200

    Added background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import logo from './logo.png';
+
 import './App.css';
+
+import background from './background.png';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
+
+      <img src={background} className="App-background" alt="background" />
+
       <header className="App-header">
 
         <a
-          href="https://www.geojokers.com"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img src={logo} className="App-logo" alt="logo" />
+        </a>
+
+        <a
+          className="App-link"
+          href="/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          www.geojokers.com
         </a>
 
         <a
